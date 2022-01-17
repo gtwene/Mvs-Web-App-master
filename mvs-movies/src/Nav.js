e66506f06d94cc2cb65a5e431ba0a1aab6dfac98
Kwame <gtwene001@st.ug.edu.gh>
2022-01-17 22:18:17 +0000

    first commit

---
+++ mvs-movies/src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToA_G6POsaEjyBug5CBTPOjSXE3t7qqC1Jsp90ltocEJvEwYH03Lh4mt_c4qh7RWcvEGw&usqp=CAU"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
